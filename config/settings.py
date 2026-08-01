370986a398a4b4c8454d0043ac1c8886eefe29db
Soheil Dutta <sohelidutta2006@gmail.com>
2026-08-02 00:14:07 +0530

    Configure allowed hosts for Render

--- config/settings.py
+++ config/settings.py
@@ -28,10 +28,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-]
+import os
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
